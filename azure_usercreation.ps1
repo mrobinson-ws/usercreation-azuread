e11342d04eab16e9b25d84a7329ff65513c31477
Mike Robinson <psikotick@gmail.com>
2021-06-08 08:18:41 -0400

    Notation Updates

--- azure_usercreation.ps1
+++ azure_usercreation.ps1
@@ -36,12 +36,11 @@ catch {
 
 #Start While Loop for Quitbox
 while ($quitboxOutput -ne "NO"){
+    ##### Create License Selection Form #####
     $licenses = Get-AzureADSubscribedSku | Select-Object -Property Sku*,ConsumedUnits -ExpandProperty PrepaidUnits
 
     $LicenseSelectWindow = New-Object System.Windows.Forms.Form
-
     $LicenseSelectWindow.Text = "Select Licenses"
-    #$LicenseSelectWindow.Size = New-Object System.Drawing.Size(500,200)
     $LicenseSelectWindow.AutoSize = $true
     $LicenseSelectWindow.AutoSizeMode = "GrowAndShrink"
     $LicenseSelectWindow.MinimizeBox = $false
@@ -63,6 +62,7 @@ while ($quitboxOutput -ne "NO"){
     $OKButton = New-Object System.Windows.Forms.Button
     $OKButton.Text = "Use Selected Licenses"
     $OKButton.Dock = [System.Windows.Forms.DockStyle]::Bottom
+    $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
     $LicenseSelectWindow.Controls.Add($OKButton)
 
     #put form in front of other windows
@@ -70,6 +70,7 @@ while ($quitboxOutput -ne "NO"){
 
     #display the form
     $DisplayForm = $LicenseSelectWindow.ShowDialog()
+
 #Create Quit Prompt and Close While Loop
 $quitboxOutput = [System.Windows.Forms.MessageBox]::Show("Do you need to create another user?" , "User Creation Complete" , 4)
 }
